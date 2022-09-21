6874b891f730eb57bb3424e3c988c94aba4778c8
Donnie Ellstrom <donnie.ellstrom@betterworks.com>
2022-09-20 20:57:33 -0700

    Adding some more problems

--- Programming Questions/LongestCommonPrefix-EASY.py
+++ Programming Questions/LongestCommonPrefix-EASY.py
@@ -25,12 +25,9 @@ class Solution:
 
         prefix = list(strs[0])
 
-        for index, string in enumerate(strs):
+        for string in enumerate(strs):
             # Convert string to list for char-to-char comparison
             current = list(string)
-            # Bail early if either list is empty
-            # if len(prefix) == 0 or len(current) == 0:
-            #     return ""
             # # Bail early if either list has a first char mismatch
             # if prefix[0] != current[0]:
             #     return ""
